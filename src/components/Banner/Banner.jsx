a391a6b7ebf424cd73257778e805b134eacfdd45
Wan Muzaffar <wanmuz86@gmail.com>
2024-07-07 10:05:48 +0800

    add banner

---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Banner.css'
+
+const Banner = () => {
+  return (
+    <div className='jumbotron text-center text-warning'>
+        <h1 className='display-4'>Shoppr</h1>
+        <p className='lead'>Shop till you drop</p>
+    </div>
+  )
+}
+
+export default Banner
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import './Home.css'
+import Banner from '../../components/Banner/Banner'
 
 const Home = () => {
   return (
+    <>
+    <Banner/>
     <div className='container py-5'>Home</div>
+    </>
   )
 }
 
